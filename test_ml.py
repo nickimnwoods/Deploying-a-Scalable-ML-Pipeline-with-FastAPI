4b2904a2daa76e382141cf6752170256c749362b
Nicki <nicki.mnwoods@gmail.com>
2026-09-05 00:52:26 +0000

    add three test cases to test_ml.py

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,55 @@
-import pytest
-# TODO: add necessary import
-
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
+from ml.model import compute_model_metrics
+from ml.model import train_model
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_compute_model_metrics():
     """
-    # add description for the second test
+    Test compute_model_metrics by entering values that should result
+    precision, recall and fbeta all equaling 1
     """
-    # Your code here
-    pass
+    y = [1, 0, 1, 0]
+    preds = [1, 0, 1, 0]
+
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+
+    assert precision == 1.0
+    assert recall == 1.0
+    assert fbeta == 1.0
 
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_verify_model():
     """
-    # add description for the third test
+    Verify train_model returns the expected algorithm of RandomForest
     """
-    # Your code here
-    pass
+    X_train = [[1], [2], [3], [4], [5]]
+    y_train = [1, 0, 1, 1, 0]
+
+    model = train_model(X_train, y_train)
+
+    assert isinstance(model, RandomForestClassifier)
+
+
+def test_train_test_split():
+    """
+    test that train_test_split produces datasets of the expected size
+    """
+    # create dummy data frame to use for test
+    data = pd.DataFrame({
+        "age": [25, 32, 47, 51, 29, 38, 44, 60, 22, 35],
+        "education": [
+            "Bachelors", "HS-grad", "Masters", "HS-grad",
+            "Bachelors", "Some-college", "Doctorate",
+            "HS-grad", "Bachelors", "Masters"
+            ],
+        "salary": [
+            "<=50K", "<=50K", ">50K", "<=50K", "<=50K",
+            "<=50K", ">50K", "<=50K", "<=50K", ">50K"
+            ]
+    })
+
+    train, test = train_test_split(data, test_size=0.20, random_state=55)
+
+    assert len(train) + len(test) == len(data)
